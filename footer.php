fb29e4fc035fa2acf8bcebfa4e3c91bc08af253d
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2023-07-15 06:56:27 -0700

    kill twitter and amg links

--- footer.php
+++ footer.php
@@ -36,7 +36,6 @@
     <li><a href="https://www.google.com/maps/place/DJ+Misha+-+San+Diego+DJ+for+Events+Party+Weddings+Club+Affordable+Prices+House+Dance+Music/@32.7990179,-117.2530949,9z/data=!4m5!3m4!1s0x0:0x3fae10d8742ab418!8m2!3d32.7990179!4d-117.2530949" class="icon brands fa-google" target="_blank" title="San Diego DJ Google +" rel="noreferrer"><span class="label">San Diego DJ Google</span></a></li>
     <li><a href="https://www.yelp.com/biz/dj-misha-san-diego-3" class="icon brands fa-yelp" target="_blank" title="San Diego DJ Yelp" rel="noreferrer"><span class="label">San Diego DJ Yelp</span></a></li>
     <li><a href="https://www.facebook.com/djmishaSandiego" class="icon brands fa-facebook" target="_blank" title="San Diego DJ Facebook" rel="noreferrer"><span class="label">San Diego DJ Facebook</span></a></li>
-    <li><a href="https://twitter.com/smblife" class="icon brands fa-twitter" target="_blank" title="San Diego DJ Twitter" rel="noreferrer"><span class="label">San Diego DJ Twitter</span></a></li>
     <li><a href="https://djmisha.com/contact-dj-misha/" class="icon" title="Email San Diego DJ" rel="noreferrer"><i class="fas fa-envelope"></i><span class="label" title="Booking San Diego DJ">Email San Diego DJ</span></a></li>
 
   </ul>
@@ -46,7 +45,6 @@
     <li>&copy; <a href="<?php home_url( '/' ); ?>" title="<?php echo get_bloginfo( 'name', 'display'  ); ?>" rel="home">
       <?php bloginfo( 'name' ); ?>
       </a> | <a href="<?php bloginfo('url'); ?>/sitemap/">Sitemap</a></li>
-    <li><a href="http://asburymediagroup.com" title="San Diego Web Design" target="_blank" rel="noopener">San Diego Web Design</a> by <a href="http://asburymediagroup.com" title="Asbury Media Group" target="_blank"  rel="noreferrer noopener">Asbury Media Group</a></li>
   </ul>
   
     <!-- Scripts -->
