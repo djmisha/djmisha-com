4e05ab3583b19ec6daf57b54aae5a5f752e127d7
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2026-04-25 04:04:56 -0700

    path for testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://djmisha.com',
-  base: '/new_djmisha', // for testing only
   output: 'static',
   trailingSlash: 'always',
   vite: {
