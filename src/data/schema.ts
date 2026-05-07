c5dcf5290976019c0deb38916e150cfca12e2867
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2026-05-07 04:38:14 -0700

    Refactor sitemap and thank-you pages for improved readability and structure
    - Updated JSON-LD structure in sitemap for better formatting.
    - Enhanced the sitemap content with clearer indentation and organization.
    - Reformatted thank-you page content for improved readability and consistency.
    - Added additional details and links to the thank-you page for user engagement.

--- src/data/schema.ts
+++ src/data/schema.ts
@@ -6,74 +6,75 @@
  * via the helper function.
  */
 
-export const SITE_NAME = "San Diego DJ for Weddings, Parties & Corporate Events – House Music, EDM & Dance";
+export const SITE_NAME =
+  'San Diego DJ for Weddings, Parties & Corporate Events – House Music, EDM & Dance';
 
 const organization = {
-  "@context": "http://schema.org/",
-  "@id": "https://djmisha.com",
-  "@type": "Organization",
-  "name": SITE_NAME,
-  "url": "https://djmisha.com",
-  "logo": {
-    "@type": "ImageObject",
-    "url": "/images/Best-San-Diego-DJ-djmisha.com-12-768x512.jpg",
-    "width": 400,
+  '@context': 'http://schema.org/',
+  '@id': 'https://djmisha.com',
+  '@type': 'Organization',
+  name: SITE_NAME,
+  url: 'https://djmisha.com',
+  logo: {
+    '@type': 'ImageObject',
+    url: '/images/Best-San-Diego-DJ-djmisha.com-12-768x512.jpg',
+    width: 400,
   },
-  "sameAs": [
-    "https://www.instagram.com/djmishasd/",
-    "https://www.youtube.com/@djmisha",
-    "https://www.facebook.com/djmishaSandiego",
-    "https://www.yelp.com/biz/dj-misha-san-diego-3",
-    "https://www.gigtown.com/artists/dj-misha",
-    "https://www.gigsalad.com/dj_misha_san_diego",
-    "https://goo.gl/maps/2rRm8kBYePqaPbVE6",
-    "https://www.zola.com/wedding-vendors/wedding-bands-djs/dj-misha",
+  sameAs: [
+    'https://www.instagram.com/djmishasd/',
+    'https://www.youtube.com/@djmisha',
+    'https://www.facebook.com/djmishaSandiego',
+    'https://www.yelp.com/biz/dj-misha-san-diego-3',
+    'https://www.gigtown.com/artists/dj-misha',
+    'https://www.gigsalad.com/dj_misha_san_diego',
+    'https://goo.gl/maps/2rRm8kBYePqaPbVE6',
+    'https://www.zola.com/wedding-vendors/wedding-bands-djs/dj-misha',
   ],
-  "contactPoint": {
-    "telephone": "+1-619-786-2664",
-    "contactType": "customer service",
+  contactPoint: {
+    telephone: '+1-619-786-2664',
+    contactType: 'customer service',
   },
 };
 
 const webSite = {
-  "@context": "http://schema.org/",
-  "@type": "WebSite",
-  "url": "https://djmisha.com",
-  "name": SITE_NAME,
-  "about":
-    "DJ Misha is San Diego DJ for parties, corporate events and weddings. Playing house music, EDM, club, dance and much more. \r\n",
+  '@context': 'http://schema.org/',
+  '@type': 'WebSite',
+  url: 'https://djmisha.com',
+  name: SITE_NAME,
+  about:
+    'DJ Misha is San Diego DJ for parties, corporate events and weddings. Playing house music, EDM, club, dance and much more. \r\n',
 };
 
 const localBusiness = {
-  "@context": "http://schema.org/",
-  "@id": "#LocalBusiness",
-  "@type": "LocalBusiness",
-  "name": SITE_NAME,
-  "url": "https://djmisha.com/",
-  "image": "/images/i-4LhQfht-L.jpg",
-  "priceRange": "Contact For Pricing",
-  "address": [
+  '@context': 'http://schema.org/',
+  '@id': '#LocalBusiness',
+  '@type': 'LocalBusiness',
+  name: SITE_NAME,
+  url: 'https://djmisha.com/',
+  image: '/images/i-4LhQfht-L.jpg',
+  priceRange: 'Contact For Pricing',
+  address: [
     {
-      "@type": "PostalAddress",
-      "streetAddress": "4640 Cass Street #9553",
-      "addressLocality": "San Diego",
-      "addressRegion": "CA",
-      "postalCode": "92109",
-      "addressCountry": "US",
-      "telephone": "+1-619-786-2664",
+      '@type': 'PostalAddress',
+      streetAddress: '4640 Cass Street #9553',
+      addressLocality: 'San Diego',
+      addressRegion: 'CA',
+      postalCode: '92109',
+      addressCountry: 'US',
+      telephone: '+1-619-786-2664',
     },
   ],
-  "employees": ["DJ Misha"],
+  employees: ['DJ Misha'],
 };
 
 const person = {
-  "@context": "http://schema.org",
-  "@type": "Person",
-  "jobTitle": "DJ",
-  "name": "DJ Misha",
-  "telephone": "+1-619-786-2664",
-  "url": "https://djmisha.com/about/",
-  "image": "/images/Best-San-Diego-DJ-djmisha.com-12-300x200.jpg",
+  '@context': 'http://schema.org',
+  '@type': 'Person',
+  jobTitle: 'DJ',
+  name: 'DJ Misha',
+  telephone: '+1-619-786-2664',
+  url: 'https://djmisha.com/about/',
+  image: '/images/Best-San-Diego-DJ-djmisha.com-12-300x200.jpg',
 };
 
 export const baseSchemas = [organization, webSite, localBusiness, person];
@@ -117,7 +118,7 @@ export function buildJsonLdWithRating(
   const orgWithRating = {
     ...organization,
     AggregateRating: {
-      "@type": "AggregateRating",
+      '@type': 'AggregateRating',
       ratingValue: String(rating.ratingValue),
       reviewCount: String(rating.reviewCount),
     },
